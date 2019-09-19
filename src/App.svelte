205d3a1f3f0fd23f10702daa3a5e626623d40370
Yannick Weiss <contact@yannickweiss.com>
2019-09-19 16:41:10 +0200

    setup informs parent of choices

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
-import 'whatwg-fetch'
+import 'whatwg-fetch';
+import Swal from 'sweetalert2';
 import { onMount } from 'svelte';
 
+import Setup from './Setup.svelte';
 import Card from './Card.svelte';
+
 // full reset or cancel of current game
 
-// storing the setup, rounds, current progress
+// storing data: {timeLimit, playerCount, rounds, currentTurn}
 
 let cards = [];
 onMount(() => {
@@ -17,15 +20,24 @@ onMount(() => {
     console.log('parsed json', json);
   }).catch(function(ex) {
     console.log('parsing failed', ex);
-  })
+	});
 });
+
+let playerCount = 4;
+let timeLimit = 60;
+
+function next(e) {
+	playerCount = e.detail.playerCount;
+	timeLimit = e.detail.timeLimit;
+
+}
 
 </script>
 
 <style>
 
-
 </style>
+<Setup on:next={next}/>
 
 {#each cards as card}
 	<Card {...card} />
--- src/Setup.svelte
+++ src/Setup.svelte
@@ -1,14 +1,24 @@
 <script>
-	let playersCount = 4;
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	let playerCount = 4;
 	let timeLimit = 60;
+
+	function next() {
+		dispatch('next', {playerCount: playerCount, timeLimit: timeLimit});
+	}
+
 </script>
 
 <h1>Monikers</h1>
-Players <input type="number" bind:value={playersCount}>
+<p>A dumb party game that respects your intelligence.</p>
+
+Players <input type="number" bind:value={playerCount}>
 Time per card <input type="number" bind:value={timeLimit}>
 <!-- hide used <input type="checkbox"-->
 
-{playersCount} {timeLimit}
+<p>Make two groups with equal amount players.</p>
 
 <a href="rules.pdf" target="_blank">How to play</a>
-<button>Start</button>
+<button on:click={next}>Start</button>
