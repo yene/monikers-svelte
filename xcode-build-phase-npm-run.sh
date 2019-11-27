d1f0335a328ffe901c9642fe23c03789c5245e0c
Yannick Weiss <contact@yannickweiss.com>
2019-11-27 11:28:58 +0100

    xcode build script for npm

---
+++ xcode-build-phase-npm-run.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+# https://stackoverflow.com/questions/6910901/how-do-i-print-a-list-of-build-settings-in-xcode-project
+# https://help.apple.com/xcode/mac/8.0/#/itcaec37c2a6
+# Use prefixes to inform Xcode: echo "warning: " or echo "error: "
+
+set -e
+# load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+if ! which node > /dev/null; then
+  echo "error: node is not installed"
+  exit 1
+fi
+
+cd $SRCROOT/..
+npm run build
